e714d344ff19bff44ba4df0f44f36fe7a5b55afe
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2019-12-04 15:08:32 +0000

    adds checked in indication

--- client/src/components/Guest.vue
+++ client/src/components/Guest.vue
@@ -2,6 +2,8 @@
   <div id="guest">
     <li>
       <h3>{{guest.name}}</h3>
+      <p>{{guest.email}}</p>
+      <p>{{ guest.status ? "Checked In" : "Still To Check In"}}</p>
       <button type="button" v-on:click="handleDelete">Remove</button>
     </li>
   </div>
