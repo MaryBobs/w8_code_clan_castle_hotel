f99aac124de6218e80e12a80838cee4aaf2a37f5
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2019-12-04 10:23:50 +0000

    app.vue created

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,11 +3,18 @@
     <div id="header">
       <h1>CodeClan Castle Hotel</h1>
     </div>
+    <new-guest-form />
   </div>
 </template>
 
 <script>
+import NewGuestForm from "./components/NewGuestForm.vue";
+
 export default {
+  name: "app",
+  components: {
+    "new-guest-form": NewGuestForm
+  }
 }
 </script>
 
