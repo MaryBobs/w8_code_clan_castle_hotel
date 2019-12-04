1d6b96f72b39723a85c396108a0c294589bd499a
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2019-12-04 10:16:00 +0000

    initial commit

---
+++ client/src/App.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+  <div id="app">
+    <div id="header">
+      <h1>CodeClan Castle Hotel</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
